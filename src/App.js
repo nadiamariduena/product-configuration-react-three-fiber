036beaf447ad580fe2ecb673df79b111728a6908
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-03-27 19:23:10 +0200

    implementing the model insid ethe code

--- src/App.js
+++ src/App.js
@@ -7,10 +7,33 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 // the useGLTF, will help us to load the model in the gltf format.
 
+/*
+
+
+
+
+*/
+
+function Model({ ...props }) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/shoe.gltf");
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  );
+}
+
 function App() {
   //
 
-  const ref = useRef();
   //
 
   return (
